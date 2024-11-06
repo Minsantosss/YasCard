dbeca171dfae68ae48153d8976c85a14bd845ead
Minsantosss <00001093859568sp@al.educacao.sp.gov.br>
2024-11-06 09:50:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem descobriu a América?',
+    'Foi Cristovão Colombo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital do Japão é Tóquio.'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são as bases nitrogenadas do DNA?',
+    'As bases são: Timina, Adenina, Guanina e Citosina.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Gato em inglês?',
+    'Gato se diz Cat em inglês.'
+)
